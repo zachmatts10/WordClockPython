d52973da247d7f8901a0a17e7548d26a328e7d70
zachmatts10 <zachmatts10@yahoo.com>
2020-03-28 17:42:41 -0400

    Create clock.py

---
+++ clock.py
@@ -0,0 +1,266 @@
+
+# File must be called clock.py !!!
+
+def half(table):
+        for n in range(2,3):
+                for m in range (24,31):
+                        table [n][m] = 1
+def quarter(table):
+        for n in range(4,5):
+                for m in range (0,13):
+                        table [n][m] = 1
+def twenty(table):
+        for n in range(4,5):
+                for m in range (14,25):
+                        table [n][m] = 1
+def ten(table):
+        for n in range(6,7):
+                for m in range (0,5):
+                        table [n][m] = 1
+def six(table):
+        for n in range(6,7):
+                for m in range (6,11):
+                        table [n][m] = 1
+def sixteen(table):
+        for n in range(6,7):
+                for m in range (6,19):
+                        table [n][m] = 1
+def two(table):
+        for n in range(6,7):
+                for m in range (20,25):
+                        table [n][m] = 1
+def one(table):
+        for n in range(6,7):
+                for m in range (24,29):
+                        table [n][m] = 1
+def eight(table):
+        for n in range(8,9):
+                for m in range (0,9):
+                        table [n][m] = 1
+def eighteen(table):
+        for n in range(8,9):
+                for m in range (0,15):
+                        table [n][m] = 1
+def five(table):
+        for n in range(8,9):
+                for m in range (16,23):
+                        table [n][m] = 1
+def seven(table):
+        for n in range(10,11):
+                for m in range (0,9):
+                        table [n][m] = 1
+def seventeen(table):
+        for n in range(10,11):
+                for m in range (0,17):
+                        table [n][m] = 1
+def nine(table):
+        for n in range(10,11):
+                for m in range (16,23):
+                        table [n][m] = 1
+def nineteen(table):
+        for n in range(10,11):
+                for m in range (16,31):
+                        table [n][m] = 1
+def four(table):
+        for n in range(12,13):
+                for m in range (0,6):
+                        table [n][m] = 1
+def fourteen(table):
+        for n in range(12,13):
+                for m in range (0,15):
+                        table [n][m] = 1
+def thirteen(table):
+        for n in range(12,13):
+                for m in range (16,31):
+                        table [n][m] = 1
+def twelve(table):
+        for n in range(14,15):
+                for m in range (0,11):
+                        table [n][m] = 1
+def eleven(table):
+        for n in range(14,15):
+                for m in range (10,21):
+                        table [n][m] = 1
+def three(table):
+        for n in range(14,15):
+                for m in range (22,31):
+                        table [n][m] = 1
+def twentyone(table):
+        for n in range(4,5):
+                for m in range (14,25):
+                        table [n][m] = 1
+        for n in range(6,7):
+                for m in range (24,29):
+                        table [n][m] = 1        
+def twentytwo(table):
+        for n in range(4,5):
+                for m in range (14,25):
+                        table [n][m] = 1
+        for n in range(6,7):
+                for m in range (20,25):
+                        table [n][m] = 1
+def twentythree(table):
+        for n in range(4,5):
+                for m in range (14,25):
+                        table [n][m] = 1
+        for n in range(14,15):
+                for m in range (22,31):
+                        table [n][m] = 1
+def twentyfour(table):
+        for n in range(4,5):
+                for m in range (14,25):
+                        table [n][m] = 1
+        for n in range(12,13):
+                for m in range (0,6):
+                        table [n][m] = 1
+def twentyfive(table):
+        for n in range(4,5):
+                for m in range (14,25):
+                        table [n][m] = 1
+        for n in range(8,9):
+                for m in range (16,23):
+                        table [n][m] = 1
+def twentysix(table):
+        for n in range(4,5):
+                for m in range (14,25):
+                        table [n][m] = 1
+        for n in range(6,7):
+                for m in range (6,11):
+                        table [n][m] = 1
+def twentyseven(table):
+        for n in range(4,5):
+                for m in range (14,25):
+                        table [n][m] = 1
+        for n in range(10,11):
+                for m in range (0,9):
+                        table [n][m] = 1
+def twentyeight(table):
+        for n in range(4,5):
+                for m in range (14,25):
+                        table [n][m] = 1
+        for n in range(8,9):
+                for m in range (0,9):
+                        table [n][m] = 1
+def twentynine(table):
+        for n in range(4,5):
+                for m in range (14,25):
+                        table [n][m] = 1
+        for n in range(10,11):
+                for m in range (16,23):
+                        table [n][m] = 1
+
+
+def two(table):
+        for n in range(18, 19):
+                for m in range (0,5):
+                        table [n][m] = 1
+def one(table):
+        for n in range(18, 19):
+                for m in range (4,9):
+                        table [n][m] = 1
+def eleven(table):
+        for n in range(18, 19):
+                for m in range (8,19):
+                        table [n][m] = 1
+def nine(table):
+        for n in range(18, 19):
+                for m in range (18,25):
+                        table [n][m] = 1
+def six(table):
+        for n in range(18, 19):
+                for m in range (26,31):
+                        table [n][m] = 1
+def seven(table):
+        for n in range(20,21):
+                for m in range (0,9):
+                        table [n][m] = 1
+def three(table):
+        for n in range(20,21):
+                for m in range (10,19):
+                        table [n][m] = 1
+def twelve(table):
+        for n in range(20,21):
+                for m in range (20,31):
+                        table [n][m] = 1
+def four(table):
+        for n in range(22,23):
+                for m in range (0,7):
+                        table [n][m] = 1
+def five(table):
+        for n in range(22,23):
+                for m in range (10,17):
+                        table [n][m] = 1
+def eight(table):
+        for n in range(22,23):
+                for m in range (16,25):
+                        table [n][m] = 1
+def ten(table):
+        for n in range(22,23):
+                for m in range (24,29):
+                        table [n][m] = 1
+
+def the(table):
+        for n in range(2,3):
+                for m in range (0,5):
+                        table [n][m] = 1
+def time(table):
+        for n in range(2,3):
+                for m in range (8,15):
+                        table [n][m] = 1
+def iss(table):
+        for n in range(2,3):
+                for m in range (18,21):
+                        table [n][m] = 1
+def minute(table):
+        for n in range(16,17):
+                for m in range (0,11):
+                        table [n][m] = 1
+def minutes(table):
+        for n in range(16,17):
+                for m in range (0,13):
+                        table [n][m] = 1
+def past(table):
+        for n in range(16,17):
+                for m in range (16,23):
+                        table [n][m] = 1
+def to(table):
+        for n in range(16,17):
+                for m in range (22,25):
+                        table [n][m] = 1
+def oclock(table):
+        for n in range(24,25):
+                for m in range (0,11):
+                        table [n][m] = 1
+def inn(table):
+        for n in range(24,25):
+                for m in range (14,17):
+                        table [n][m] = 1
+def at(table):
+        for n in range(24,25):
+                for m in range (18,21):
+                        table [n][m] = 1
+def night(table):
+        for n in range(26,27):
+                for m in range (0,9):
+                        table [n][m] = 1
+def the(table):
+        for n in range(26,27):
+                for m in range (8,15):
+                        table [n][m] = 1
+def morning(table):
+        for n in range(26,27):
+                for m in range (16,29):
+                        table [n][m] = 1
+def evening(table):
+        for n in range(28,29):
+                for m in range (0,13):
+                        table [n][m] = 1
+def afternoon(table):
+        for n in range(28,29):
+                for m in range (14,31):
+                        table [n][m] = 1
+def noon(table):
+        for n in range(28,29):
+                for m in range (24,31):
+                        table [n][m] = 1
+                        
