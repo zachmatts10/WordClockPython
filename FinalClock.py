14d9fc1ccc6f12a6cbd6c32614850bcdafb75198
zachmatts10 <zachmatts10@yahoo.com>
2020-03-29 12:36:41 -0400

    Update FinalClock.py

--- FinalClock.py
+++ FinalClock.py
@@ -6,7 +6,7 @@ import datetime
 
 clockListMinute = [clock.one, clock.two, clock.three, clock.four, clock.five, clock.six, clock.seven, clock.eight, clock.nine, clock.ten, clock.eleven, clock.twelve, clock.thirteen, clock.fourteen, clock.quarter, clock.sixteen, clock.seventeen, clock.eighteen, clock.nineteen, clock.twenty, clock.twentyone, clock.twentytwo, clock.twentythree, clock.twentyfour, clock.twentyfive, clock.twentysix, clock.twentyseven, clock.twentyeight, clock.twentynine, clock.half] #0-29
 clockListHour = [clock.Hone, clock.Htwo, clock.Hthree, clock.Hfour, clock.Hfive, clock.Hsix, clock.Hseven, clock.Height, clock.Hnine, clock.Hten, clock.Heleven, clock.Htwelve] #0-11
-clockListAdd = [clock.the, clock.time, clock.iss, clock.minute, clock.minutes, clock.past, clock.to, clock.oclock, clock.inn, clock.at, clock.night, clock.the2, clock.morning, clock.evening, clock.afternoon, clock.noon] #0-15
+clockListAdd = [clock.the, clock.time, clock.iss, clock.minute, clock.minutes, clock.past, clock.to, clock.oclock, clock.inn, clock.at, clock.night, clock.the2, clock.morning, clock.evening, clock.afternoon, clock.noon, clock.anniversary, clock.birthday] #0-17
 
 class FinalClock(SampleBase):
     def __init__(self, *args, **kwargs):
@@ -21,18 +21,33 @@ class FinalClock(SampleBase):
             hour = time.strftime ("%H")
             minute = time.strftime ("%M")
             second = time.strftime ("%S")
+            day = time.strftime ("%d")
+            month = time.strftime ("%m")
             # Make the whole thing available as an integer
             hour = int (hour)
             minute = int (minute)
             second = int (second)
+            day = int (day)
+            month = int (month)
             print (hour, minute, second)
             # Create an empty table
             table = [[0 for g in range (33)] for h in range (33)]
+            
+            Anniversary = 29
+            BirthDay = 29
+            BirthMonth = 3
+            
+            
             # It is always lit.
             clockListAdd [0] (table)
             clockListAdd [1] (table)
             clockListAdd [2] (table)
 
+            if (day == Anniversary):
+                    clockListAdd [16] (table)
+            elif (month == BirthMonth and day == BirthDay):
+                    clockListAdd [17] (table)
+                      
             if (minute <= 30):
                     if (minute==0):
                             clockListAdd [7] (table)
